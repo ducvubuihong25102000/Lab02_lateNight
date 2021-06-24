1d33f8500c6d9fba6b93c1f7bca5ede34f49943a
Duc toc dai <ducvubuihong25102000@gmail.com>
2021-06-24 21:06:14 +0700

    Add project files.

---
+++ Controllers/BooksController.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using Lab02;
+using Lab02.Models;
+
+namespace Lab02.Controllers
+{
+    public class BooksController : Controller
+    {
+        // GET: Books
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        private List<Book> ListBooks;
+        public BooksController()
+        {
+            ListBooks = new List<Book>();
+            ListBooks.Add(new Book(1,"Tôi Thấy Hoa Vàng Trên Cỏ Xanh","Nguyễn Nhật Ánh","Content/Images/ToiThayHoaVangTrenCoXanh.jpg",100000));
+            ListBooks.Add(new Book(1, "Cho Tôi Một Vé Đi Tuổi Thơ", "Nguyễn Nhật Ánh", "Content/Images/ChoToiMotVeDiTuoiTho.jpg", 100000));
+            ListBooks.Add(new Book(1, "Rừng Na Uy", "Hikaruma", "Content/Images/RungNauy.jpg", 125000));
+            ListBooks.Add(new Book(1, "Du Hành Vào Xứ Phẳng", "Adam", "Content/Images/XuPhang.jpg", 80000));
+            ListBooks.Add(new Book(1, "Harry Potter và chiếc cốc lửa", "JK.Rowling", "Content/Images/ChiecCocLua.jpg", 168000));
+        }
+
+        public ActionResult ListBooks_()
+        {
+            ViewBag.TitlePageName = "Book view page";
+            return View(ListBooks);
+        }
+
+        public ActionResult Detail(int? id)
+        {
+            if (id == null)
+                return HttpNotFound();
+            Book book = ListBooks.Find(s => s.Id == id);
+            if (book == null)
+                return HttpNotFound();
+
+            return View(book);
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+                return HttpNotFound();
+            Book book = ListBooks.Find(s => s.Id == id);
+
+            if (book == null)
+                return HttpNotFound();
+            return View(book);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Book book)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var editbook = ListBooks.Find(b => b.Id == book.Id);
+                    editbook.BookName = book.BookName;
+                    editbook.Author = book.Author;
+                    editbook.Price = book.Price;
+                    return View("ListBooks_", ListBooks);
+                }
+                catch(Exception e)
+                {
+                    return HttpNotFound();
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("", "Input Model not valid");
+                return View(book);
+            }
+            return View();
+        }
+    }
+}
---
+++ Models/Book.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Lab02.Models
+{
+    public class Book
+    {
+        public Book(int id, string bookName, string author,  string img, double price)
+        {
+            Id = id;
+            BookName = bookName;
+            Author = author;
+            this.Price = price;
+            Img = img;
+        }
+        public Book() { }
+
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string BookName { get; set; }
+        [Required]
+        public string Author { get; set; }
+        [Required]
+        public double Price { get; set; }
+        public string Img { get; set; }
+    }
+}
